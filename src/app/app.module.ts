5ffd0e872a81f6c314ec9e256f7dfc111626631b
attaporn sophonslidsuk <sophonslidsuk@yahoo.com>
2020-04-09 09:35:05 +0700

    Update app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core' ; 
+import { BrowserModule } from '@angular/platform-browser'; 
+import { AppComponent } from './app.AppComponent';
+
+@NgModule({
+    imports:[
+        BrowserModule
+    ],
+    declarations:[
+        AppComponent
+    ],
+    bootstrap:[]
+})
+
+
+export class AppModule {}
